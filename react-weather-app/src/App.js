9cc0aff7080125946f672213694af980bdedf9a6
Mitskevich Victor <mv2.mx@yandex.ru>
2019-08-21 13:26:52 +0300

    feat: add search form

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchWeather from './Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MainPage extends React.Component {
+  render() {
+    return (
+      <>
+        <form>
+          <label>
+            Sity:
+            <input type='text' name='Sity' />
+          </label>
+          <input type='submit' value='Search' onClick='SearchWeather'/>
+        </form>
+      </>
+    )
+  }
 }
 
-export default App;
+export default MainPage;
